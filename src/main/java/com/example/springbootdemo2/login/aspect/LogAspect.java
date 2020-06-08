45804b8cebc52707c24da5f75c54c19a985649c2
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-09 08:44:44 +0900

    コントローラーが呼ばれたときログを出すようにした(AOPの学習)

---
+++ src/main/java/com/example/springbootdemo2/login/aspect/LogAspect.java
@@ -0,0 +1,35 @@
+package com.example.springbootdemo2.login.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+  
+  @Around("@within(org.springframework.stereotype.Controller)")
+  public Object startLog(ProceedingJoinPoint jp) throws Throwable{
+
+    System.out.println("メソッド開始: " + jp.getSignature());
+
+    try {
+
+      Object result = jp.proceed();
+
+      System.out.println("メソッド終了: " + jp.getSignature());
+
+      return result;
+    } catch (Exception e) {
+
+      System.out.println("メソッド異常終了" + jp.getSignature());
+      e.printStackTrace();
+      throw e;
+    }
+
+
+  }
+
+
+}
