095df311e8da4d2bf19b2eba8e2ca0c24c8cb95b
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-10 08:28:46 +0900

    HomeController作成

---
+++ src/main/java/com/example/springbootdemo2/login/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.springbootdemo2.login.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+  
+  @Autowired
+  UserService userService;
+
+  @GetMapping("/home")
+  public String getHome(Model model) {
+
+    model.addAttribute("contents", "login/home::home_contents");
+
+    return "login/homeLayout.html";
+  }
+
+  @PostMapping("/logout")
+  public String postLogout() {
+
+    return "redirect:/login";
+  }
+}
