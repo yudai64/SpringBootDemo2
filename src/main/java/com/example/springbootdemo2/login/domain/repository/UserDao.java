f1ea5de9d76a1265e10e1d1da7f44c3e13ff05e9
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-10 10:07:37 +0900

    戻り値の型の修正

--- src/main/java/com/example/springbootdemo2/login/domain/repository/UserDao.java
+++ src/main/java/com/example/springbootdemo2/login/domain/repository/UserDao.java
@@ -1,5 +1,7 @@
 package com.example.springbootdemo2.login.domain.repository;
 
+import java.util.List;
+
 import com.example.springbootdemo2.login.domain.model.User;
 
 import org.springframework.dao.DataAccessException;
@@ -7,7 +9,6 @@ import org.springframework.dao.DataAccessException;
 public interface UserDao {
   
   public int count() throws DataAccessException;
-
 
   // Userテーブルにデータを一件挿入
   public int insertOne(User user) throws DataAccessException;
@@ -17,7 +18,7 @@ public interface UserDao {
 
 
     //Userテーブルのデータを全件取得
-    public int selectMany() throws DataAccessException;
+    public List<User> selectMany() throws DataAccessException;
 
   //Userテーブルのデータを一件更新
   public int updateOne(User user) throws DataAccessException;
