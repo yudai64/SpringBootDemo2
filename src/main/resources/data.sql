783de1240e8a4f1a1503a37ff0bfe3c59a28104f
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-11 07:02:41 +0900

    Userテーブルとデータの作成

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
+-- 従業員テーブルのデータ
 INSERT INTO employee (employee_id, employee_name, age)
 VALUES(1, '山田太郎', 30);
+
+-- ユーザーマスタのデータ
+INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
+VALUES('yamada@example.com', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
