cdb5a3988342db65c141fef168567ce956c00831
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-13 17:54:56 +0900

    selectManyメッソドをオーバーライドした

--- src/main/java/com/example/springbootdemo2/login/domain/repository/jdbc/UserDaoJdbcImpl2.java
+++ src/main/java/com/example/springbootdemo2/login/domain/repository/jdbc/UserDaoJdbcImpl2.java
@@ -1,9 +1,12 @@
 package com.example.springbootdemo2.login.domain.repository.jdbc;
 
+import java.util.List;
+
 import com.example.springbootdemo2.login.domain.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository("UserDaoJdbcImpl2")
@@ -20,5 +23,15 @@ public class UserDaoJdbcImpl2 extends UserDaoJdbcImpl {
     UserRowMapper rowMapper = new UserRowMapper();
 
     return jdbcTemplate.queryForObject(sql, rowMapper, userId);
+  }
+
+  @Override
+  public List<User> selectMany() {
+
+    String sql = "SELECT * FROM m_user";
+
+    RowMapper<User> rowMapper = new UserRowMapper();
+
+    return jdbcTemplate.query(sql, rowMapper);
   }
 }
