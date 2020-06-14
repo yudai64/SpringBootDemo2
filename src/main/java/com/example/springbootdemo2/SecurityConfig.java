22a9c29ac21ad9d5172f6aa23b2c7a4ca82e70fc
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-15 08:16:35 +0900

    権限によってアクセスを制限

--- src/main/java/com/example/springbootdemo2/SecurityConfig.java
+++ src/main/java/com/example/springbootdemo2/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
             .antMatchers("/css/**").permitAll()
             .antMatchers("/login").permitAll()
             .antMatchers("/signup").permitAll()
+            .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
             .anyRequest().authenticated()
 
         .and()
