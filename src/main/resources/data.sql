167f6c59e0b64bdde01310768d69305be93ed323
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-14 15:14:35 +0900

    一般権限のユーザー追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,10 @@
 INSERT INTO employee (employee_id, employee_name, age)
 VALUES(1, '山田太郎', 30);
 
--- ユーザーマスタのデータ
+-- ユーザーマスタのデータ(アドミン権限)
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
 VALUES('yamada@example.com', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
+
+-- ユーザーマスタのデータ(一般権限)
+INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
+VALUES('tamura@example.com', 'password', '田村達也', '1986-11-05', 31, false, 'ROLE_GENERAL');
