6d909b563033f299fb20d539c9ecf6ea33b0f584
Yudai Yano <kemurin.yudai0723@gmail.com>
2020-06-14 17:20:56 +0900

    パスワードを復号化した状態に修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ VALUES(1, '山田太郎', 30);
 
 -- ユーザーマスタのデータ(アドミン権限)
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
-VALUES('yamada@example.com', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
+VALUES('yamada@example.com', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1hulPa', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
 
 -- ユーザーマスタのデータ(一般権限)
 INSERT INTO m_user(user_id, password, user_name, birthday, age, marriage, role)
-VALUES('tamura@example.com', 'password', '田村達也', '1986-11-05', 31, false, 'ROLE_GENERAL');
+VALUES('tamura@example.com', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1hulPa', '田村達也', '1986-11-05', 31, false, 'ROLE_GENERAL');
